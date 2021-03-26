a6f0c815e43128287e9bd632e3e516daa5f4fcd0
Jimmy H <jim.eho97@gmail.com>
2021-03-25 23:39:01 -0700

    port to Windows + bugfix

--- kg_downloader.py
+++ kg_downloader.py
@@ -20,8 +20,14 @@ options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--incognito')
 options.add_argument('--headless')
-driver = webdriver.Chrome(
-    "/usr/lib/chromium-browser/chromedriver", options=options)
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.add_argument('--allow-insecure-localhost')
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+chrome_driver_binary = "E:\\Chromedriver\\chromedriver.exe"
+driver = webdriver.Chrome(chrome_driver_binary, options=options)
 
 
 def main():
@@ -29,8 +35,8 @@ def main():
 
     # Grab kg-url input from user
 
-    # acc_url = input("Enter K歌 Account URL: ")
-    acc_url = "https://node.kg.qq.com/personal?uid=6b9c9c83202d308b35"
+    acc_url = input("Enter K歌 Account URL: ")
+    # acc_url = "https://node.kg.qq.com/personal?uid=6b9c9c83202d308b35" # Mom's
 
     # Use Selenium to allow full loading of page
     driver.get(acc_url)
@@ -42,7 +48,7 @@ def main():
     print("URL parsed...")
 
     # Provision file structure
-    provision(BeautifulSoup(driver.page_source, 'lxml'))
+    provision(BeautifulSoup(driver.page_source, 'html.parser'))
 
 
 def provision(soup):
@@ -65,13 +71,12 @@ def provision(soup):
     for link in album_links:
         # Create soup for album page
         req = requests.get(link, HEADERS)
-        album_soup = BeautifulSoup(req.content, 'lxml')
+        album_soup = BeautifulSoup(req.content, 'html.parser')
 
         # path for album
         album_name = album_soup.find("h2", {"class": "play_name"}).next
 
         print("---Album Name: {}---".format(album_name))
-
         song_extract(album_soup, acc_file_path)
 
 
@@ -84,11 +89,17 @@ def song_extract(album_soup, acc_file_path):
         # Create soup for song page w/ Selenium
         driver.get(link)
         time.sleep(1)  # Allow JS to fully load
-        song_soup = BeautifulSoup(driver.page_source, "lxml")
+        song_soup = BeautifulSoup(driver.page_source, "html.parser")
 
         audio_elem = song_soup.find("audio")
         audio_src = audio_elem['src']
         audio_name = "{0}.mp3".format(audio_elem['meta'])
+
+        # Changes audio_name based on special characters
+        if ":" in audio_name or "/" in audio_name:
+            audio_name = audio_name.replace(":", "-")
+            audio_name = audio_name.replace("/", "-")
+
         audio_path = os.path.join(acc_file_path, audio_name)
 
         print("Song: {}...".format(audio_name), end="")
@@ -96,7 +107,7 @@ def song_extract(album_soup, acc_file_path):
         # Skip if song already exists
         if not os.path.exists(audio_path):
             mp3_file = requests.get(audio_src, allow_redirects=True)
-            with open(os.path.join(acc_file_path, audio_path), 'wb') as f:
+            with open(audio_path, 'wb') as f:
                 f.write(mp3_file.content)
             print("100%")
         else:
@@ -107,10 +118,10 @@ if __name__ == "__main__":
     # # Test
     # links = ['https://node.kg.qq.com/album?s=6b9c9c83202d308b352176d9b3c8cfcf6fb1f69cd8b9efa698bf4a8b1281848c36d2d13c81e170d691&g_f=personal&appsource=', 'https://node.kg.qq.com/album?s=6b9c9c83202d308b352176d9b3c8cfcf6cb8f799d8bbeaa09cbe408e128e828c36d2d13c81e170d691&g_f=personal&appsource=', 'https://node.kg.qq.com/album?s=6b9c9c83202d308b352176d9b3c8cfcf6cb9f39ed8beeca39bbf4e821281ec800ad1d212acef67ce&g_f=personal&appsource=', 'https://node.kg.qq.com/album?s=6b9c9c83202d308b352176d9b3c8cfcf6cb4fa9fd8bdeea198b94d8b1686ec800ad1d212acef67ce&g_f=personal&appsource=',
     #          'https://node.kg.qq.com/album?s=6b9c9c83202d308b352176d9b3c8cfcf6cb3fb9fd8b9e4ad9cbc4b8b1585878c36d2d13c81e170d691&g_f=personal&appsource=', 'https://node.kg.qq.com/album?s=6b9c9c83202d308b352176d9b3c8cfcf68b8f495d8bfe4a39dbe488f1687ec800ad1d212acef67ce&g_f=personal&appsource=', 'https://node.kg.qq.com/album?s=6b9c9c83202d308b352176dabbcec2cb6cb2f49dd8beeca39eb041831780878c36d2d13c81e170d691&g_f=personal&appsource=', 'https://node.kg.qq.com/album?s=6b9c9c83202d308b352176dab5c9cfce6db4f599d8bbeca69eb8408d118e848c36d2d13c81e170d691&g_f=personal&appsource=']
-    
+
     # for link in links:
     #     req = requests.get(link, HEADERS)
-    #     album_soup = BeautifulSoup(req.content, 'lxml')
+    #     album_soup = BeautifulSoup(req.content, 'html.parser')
 
     #     song_extract(album_soup, OUTPUT_PATH)
 
